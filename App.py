02002bbfa76806dd738804de3cf4c9bc416b8014
Hugues <hugues.farthouat@cpe.fr>
2022-01-10 15:08:26 +0100

    Auto stash before merge of "main" and "origin/main"

--- App.py
+++ App.py
@@ -24,7 +24,11 @@ class App:
 
         self.configObject = config.Config('config.ini')
         self.config = self.configObject.read_config()
+<<<<<<< Updated upstream
         
+=======
+
+>>>>>>> Stashed changes
         self.menu = "menu"
 
         self.gameFrame = IFrame.GameFrame(self)
